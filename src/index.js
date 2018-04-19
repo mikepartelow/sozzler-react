7ddf20b5a161b118fc50c4f5f31c271bd9f65aea
Mike Partelow <github@mikepartelow.com>
2018-04-18 21:36:01 -0700

    nice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Helmet} from "react-helmet";
 import './index.css';
-import { List, Container, Search, Menu, Divider } from 'semantic-ui-react';
+import { List, Container, Search, Menu, Divider, Image } from 'semantic-ui-react';
+import { recipes } from './recipes';
 import 'semantic-ui-css/semantic.min.css';
-
-function Helmet(props) {
-  return ''
-}
+import _ from 'lodash';
 
 function fancy_quantity(quantity) {
   let [qn, qd] = quantity.split('/')
@@ -26,17 +23,30 @@ function fancy_quantity(quantity) {
   return quantity
 }
 
+function OliveRating(props) {
+  return (
+    <span>
+      {_.times(props.rating, i => (
+        <img key={i} src='./olive-black-1.png' width={props.width} alt={i}/>
+      ))}
+      {_.times(5-props.rating, i => (
+        <img key={i} src='./olive-white-1.png' width={props.width} alt=''/>
+      ))}      
+    </span>
+  )
+}
+
 function RecipeDetails(props) {
   return (
     <div>
-      <Helmet>
-        <title>{props.recipe.name}</title>
-      </Helmet>
-
       <div className="recipe-details">
         <div className="name">
-          <h3>{props.recipe.name} : {props.recipe.rating}</h3>
+          <h2>{props.recipe.name}</h2>
+          <OliveRating rating={props.recipe.rating} width="25"/>
         </div>
+      
+        <Divider />
+
         {props.recipe.components.sort((c0, c1) => c0.index > c1.index).map((c) =>        
           <div key={c.index} className="component">
             <span className="quantity">{fancy_quantity(c.quantity)} </span>
@@ -57,14 +67,13 @@ function RecipeList(props) {
   let recipes = props.recipes.sort((a, b) => b.rating - a.rating || a.name.localeCompare(b.name))
                              .filter((recipe) => recipe.name.toLowerCase().includes(props.filterText.toLowerCase()))
   
-  // <Helmet><title>{this.props.ingredient ? "Recipes With " + this.props.ingredient : "Recipes"}</title></Helmet>
-    
   return (
     <List divided relaxed>
       {recipes.map(recipe =>
-        <List.Item key={recipe.name} onClick={() => props.onRecipeSelected(recipe)}>
+        <List.Item as='a' key={recipe.name} onClick={() => props.onRecipeSelected(recipe)}>
             <List.Content>
-              <List.Header>{recipe.name} : {recipe.rating}</List.Header>
+              <List.Header>{recipe.name}</List.Header>
+              <List.Header><OliveRating rating={recipe.rating} width="15"/></List.Header>
               <List.Description>
                 {recipe.components.sort((c0, c1) => c0.index > c1.index).map(c => c.ingredient).join(', ')}
               </List.Description>
@@ -93,9 +102,9 @@ class FilteredRecipeList extends React.Component {
 function IngredientList(props) {
   return (
     <List divided relaxed>
-      {Array.from(props.ingredients).map(([ingredient, recipes]) => {
+      {Array.from(props.ingredients).sort((a, b) => a[0].localeCompare(b[0])).map(([ingredient, recipes]) => {
         return (
-          <List.Item key={ingredient} onClick={() => props.onIngredientSelected(ingredient, recipes)}>
+          <List.Item as='a' key={ingredient} onClick={() => props.onIngredientSelected(ingredient, recipes)}>
             <List.Content>
               <List.Header>{ingredient}</List.Header>
               <List.Description>{recipes.map(r => r.name).sort().join(', ')}</List.Description>
@@ -150,10 +159,16 @@ class App extends React.Component {
 
     return (
       <Container>
-        <Menu items={[
-          { key: 'recipes', name: 'Recipes', active: !this.state.ingredientView, onClick: this.handleRecipes },
-          { key: 'ingredients', name: 'Ingredients', active: this.state.ingredientView, onClick: this.handleIngredients}
-        ]} />      
+        <Menu borderless={true} fluid widths={2}>
+          <Menu.Item name='Recipes' active={!this.state.ingredientView} onClick={this.handleRecipes}>
+            <Image src='./Martini-outline-Small@2x.png' width='25'/>
+            &nbsp;Recipes
+          </Menu.Item>
+          <Menu.Item name='Ingredients' active={this.state.ingredientView} onClick={this.handleIngredients}>
+            <Image src='./Bottles-label-Small@2x.png' width='25'/>
+            &nbsp;&nbsp;Ingredients
+          </Menu.Item>
+        </Menu>      
         {view}
       </Container>
     )
@@ -163,20 +178,26 @@ class App extends React.Component {
 // <MenuBar onRecipes={this.handleRecipes} onIngredients={this.handleIngredients} />
 
 // ========================================
-let url = 'https://raw.githubusercontent.com/mikepartelow/sozzler-recipes/master/SozzlerApp/1.0.sozzler';
-fetch(url)
-  .then((res) => res.json())
-  .then((recipes) => {
-    var ingredients = new Map();
+function gitrdone(recipes) {
+  var ingredients = new Map();
 
-    recipes.map((recipe) =>
-      recipe.components.map((component) => {
-        var ingredient_recipes = ingredients.get(component.ingredient) || [];
-        ingredient_recipes.push(recipe);
-        ingredients.set(component.ingredient, ingredient_recipes);
-        return null;
-      })
-    )
+  recipes.map((recipe) =>
+    recipe.components.map((component) => {
+      var ingredient_recipes = ingredients.get(component.ingredient) || [];
+      ingredient_recipes.push(recipe);
+      ingredients.set(component.ingredient, ingredient_recipes);
+      return null;
+    })
+  )
 
-    ReactDOM.render(<App recipes={recipes} ingredients={ingredients} />, document.getElementById('root'));
-})
+  ReactDOM.render(<App recipes={recipes} ingredients={ingredients} />, document.getElementById('root'));
+}
+
+// let url = 'https://raw.githubusercontent.com/mikepartelow/sozzler-recipes/master/SozzlerApp/1.0.sozzler';
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((recipes) => {
+//     gitrdone(recipes)
+// })
+
+gitrdone(recipes)
