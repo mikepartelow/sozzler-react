ac93d86f23a3155fc3250921325122c9d653eb6d
Mike Partelow <github@mikepartelow.com>
2018-05-01 23:45:06 -0700

    slug

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM node
 # RUN create-react-app sozzler
 # RUN rm -f my-app/src/*
 
-# RUN yarn add react react-dom semantic-ui-react semantic-ui-css react-helmet
+RUN yarn add react react-dom semantic-ui-react semantic-ui-css react-helmet react-router-dom slug
 
 WORKDIR /sozzler/
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { List, Container, Search, Menu, Divider, Image } from 'semantic-ui-react';
-import {Helmet} from "react-helmet";
-import { recipes } from './recipes';
-import 'semantic-ui-css/semantic.min.css';
-import _ from 'lodash';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import slug from 'slug'
+import './index.css'
+import { List, Container, Search, Menu, Divider, Image, Segment } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import {Helmet} from "react-helmet"
+import { recipes } from './recipes'
+import 'semantic-ui-css/semantic.min.css'
+import _ from 'lodash'
 
 function fancy_quantity(quantity) {
   let [qn, qd] = quantity.split('/')
@@ -28,191 +31,134 @@ function OliveRating(props) {
   return (
     <span>
       {_.times(props.rating, i => (
-        <img key={i} src='./olive-black-1.png' width={props.width} alt={i}/>
+        <Image inline key={i} src='/olive-black-1.png' width={props.width} alt={i}/>
       ))}
       {_.times(5-props.rating, i => (
-        <img key={i} src='./olive-white-1.png' width={props.width} alt=''/>
+        <Image inline key={i} src='/olive-white-1.png' width={props.width} alt=''/>
       ))}
     </span>
   )
 }
 
-function RecipeDetails(props) {
-  return (
-    <div>
-      <Helmet>
-        <title>Sozzler: {props.recipe.name}</title>
-      </Helmet>
+const Recipe = (props) => (
+  <Segment>
+    <Helmet>
+      <title>Sozzler: {props.recipe.name}</title>
+    </Helmet>
 
-      <div className="recipe-details">
-        <div className="name">
-          <h2>{props.recipe.name}</h2>
-          <OliveRating rating={props.recipe.rating} width="25"/>
-        </div>
+    <Container>
+      <h2>{props.recipe.name}</h2>
+      <OliveRating rating={props.recipe.rating} width="25"/>
+    </Container>
 
-        <Divider />
+    <Divider />
 
-        {props.recipe.components.sort((c0, c1) => c0.index > c1.index).map((c) =>
-          <div key={c.index} className="component">
-            <span className="quantity">{fancy_quantity(c.quantity)} </span>
-            <span className="unit">{c.unit} </span>
-            <span className="ingredient">{c.ingredient}</span>
-          </div>
-        )}
-        <Divider />
-        <div className="text">
-          {props.recipe.text}
-        </div>
-      </div>
-    </div>
-  );
-}
+    {props.recipe.components.sort((c0, c1) => c0.index > c1.index).map((c) =>
+      <Container key={c.index} className="component">
+        <span className="quantity">{fancy_quantity(c.quantity)} </span>
+        <span className="unit">{c.unit} </span>
+        <span className="ingredient">{c.ingredient}</span>
+      </Container>
+    )}
+    <Divider />
+    <Container className="text">
+      {props.recipe.text}
+    </Container>
+  </Segment>
+)
 
-function RecipeList(props) {
-  let recipes = props.recipes.sort((a, b) => b.rating - a.rating || a.name.localeCompare(b.name))
-                             .filter((recipe) => recipe.name.toLowerCase().includes(props.filterText.toLowerCase()))
+const iincludes = (a, b) => a.toLowerCase().includes(b.toLowerCase())
 
-  return (
-    <List divided relaxed>
-      {recipes.map(recipe =>
-        <List.Item as='a' key={recipe.name} onClick={() => props.onRecipeSelected(recipe)}>
-            <List.Content>
-              <List.Header as='h3'>{recipe.name}</List.Header>
-              <List.Header><OliveRating rating={recipe.rating} width="15"/></List.Header>
-              <List.Description>
-                {recipe.components.sort((c0, c1) => c0.index > c1.index).map(c => c.ingredient).join(', ')}
-              </List.Description>
-            </List.Content>
-          </List.Item>
-      )}
-    </List>
-  )
-}
-
-class FilteredRecipeList extends React.Component {
+class Recipes extends React.Component {
   state = {filterText: ''}
-
   handleFilterTextChange = (e, { value }) => this.setState({filterText: value})
 
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Sozzler: Recipes</title>
-        </Helmet>
-
-        <Search input={{ fluid: true }} size={'mini'} open={false} onSearchChange={this.handleFilterTextChange} />
-        <RecipeList recipes={this.props.recipes} filterText={this.state.filterText} onRecipeSelected={this.props.onRecipeSelected} />
-      </div>
-    )
-  }
-}
-
-function IngredientList(props) {
-  return (
-    <div>
-      <Helmet>
-        <title>Sozzler: Ingredients</title>
-      </Helmet>
-
+  render = () => (
+    <Container>
+      <Search input={{ fluid: true }} size={'mini'} open={false} onSearchChange={this.handleFilterTextChange} />
       <List divided relaxed>
-        {Array.from(props.ingredients).sort((a, b) => a[0].localeCompare(b[0])).map(([ingredient, recipes]) => {
-          return (
-            <List.Item as='a' key={ingredient} onClick={() => props.onIngredientSelected(ingredient, recipes)}>
+        {this.props.recipes.sort((a, b) => b.rating - a.rating || a.name.localeCompare(b.name))
+                      .filter((recipe) => iincludes(recipe.name, this.state.filterText))
+                      .map((recipe) =>
+          <List.Item as={Link} to={`/recipes/${recipe.slug}`} key={recipe.slug}>
               <List.Content>
-                <List.Header>{ingredient}</List.Header>
-                <List.Description>{recipes.map(r => r.name).sort().join(', ')}</List.Description>
+                <List.Header as='h3'>{recipe.name}</List.Header>
+                <List.Header><OliveRating rating={recipe.rating} width="15"/></List.Header>
+                <List.Description>
+                  {recipe.components.sort((c0, c1) => c0.index > c1.index).map(c => c.ingredient).join(', ')}
+                </List.Description>
               </List.Content>
             </List.Item>
-          )
-        })}
+        )}
       </List>
-    </div>
+    </Container>
   )
 }
 
-class App extends React.Component {
-  state = {
-    ingredient: null,
-    recipes: null,
-    selectedRecipe: null,
-    ingredientView: false,
-  }
+const Ingredients = (props) => (
+  <Container>
+    <Helmet>
+      <title>Sozzler: Ingredients</title>
+    </Helmet>
 
-  handleRecipes = (e, d) => {
-    this.setState({
-      ingredient: null,
-      recipes: null,
-      selectedRecipe: null,
-      ingredientView: false,
-    });
-  }
+    <List divided relaxed>
+      {Array.from(props.ingredients).sort((a, b) => a[0].localeCompare(b[0])).map(([ingredient, recipes]) => {
+        return (
+          <List.Item as='a' key={ingredient}>
+            <List.Content>
+              <List.Header>{ingredient}</List.Header>
+              <List.Description>{recipes.map(r => r.name).sort().join(', ')}</List.Description>
+            </List.Content>
+          </List.Item>
+        )
+      })}
+    </List>
+  </Container>
+)
 
-  handleIngredients = (e, d) => this.setState({ingredientView: true})
+const Sozzler = (props) => (
+  <Router>
+    <Container>
+      <Menu borderless={true} fluid widths={2}>
+        <Menu.Item name='Recipes' as={Link} to='/recipes' onClick={this.handleRecipes}>
+          <Image src='/Martini-outline-Small@2x.png' width='25'/>
+          &nbsp;Recipes
+        </Menu.Item>
+        <Menu.Item name='Ingredients' as={Link} to='/ingredients' onClick={this.handleIngredients}>
+          <Image src='/Bottles-label-Small@2x.png' width='25'/>
+          &nbsp;&nbsp;Ingredients
+        </Menu.Item>
+      </Menu>
 
-  handleRecipeSelected = (selectedRecipe) => this.setState({selectedRecipe: selectedRecipe})
+      <Route path='/recipes/:slug' render={(rprops) => (
+        <Recipe recipe={props.recipes.find((r) => r.slug === rprops.match.params.slug)}/>
+      )}/>
+      <Route exact path="/recipes" render={() => <Recipes recipes={props.recipes}/>}/>
+      <Route path="/ingredients" render={() => <Ingredients ingredients={props.ingredients}/>}/>
+    </Container>
+  </Router>
+)
 
-  handleIngredientSelected = (ingredient, recipes) => {
-    this.setState({
-      ingredient: ingredient,
-      recipes: recipes,
-      selectedRecipe: null,
-      ingredientView: false,
-    })
-  }
+// TODO: restore 'active' on menu.item
+// TODO: ingredients page works
+// TODO: Helmet: Sozzler: Recipes
 
-  render() {
-    var view;
-
-    if (this.state.ingredientView) {
-      view = <IngredientList ingredients={this.props.ingredients} onIngredientSelected={this.handleIngredientSelected} />
-    } else if (this.state.selectedRecipe) {
-      view = <RecipeDetails recipe={this.state.selectedRecipe} />
-    } else {
-      view = <FilteredRecipeList recipes={this.state.recipes || this.props.recipes} ingredient={this.state.ingredient} onRecipeSelected={this.handleRecipeSelected} />
-    }
-
-    return (
-      <Container>
-        <Menu borderless={true} fluid widths={2}>
-          <Menu.Item name='Recipes' active={!this.state.ingredientView} onClick={this.handleRecipes}>
-            <Image src='./Martini-outline-Small@2x.png' width='25'/>
-            &nbsp;Recipes
-          </Menu.Item>
-          <Menu.Item name='Ingredients' active={this.state.ingredientView} onClick={this.handleIngredients}>
-            <Image src='./Bottles-label-Small@2x.png' width='25'/>
-            &nbsp;&nbsp;Ingredients
-          </Menu.Item>
-        </Menu>
-        {view}
-      </Container>
-    )
-  }
-}
-
-// <MenuBar onRecipes={this.handleRecipes} onIngredients={this.handleIngredients} />
-
-// ========================================
 function gitrdone(recipes) {
-  var ingredients = new Map();
+  var ingredients = new Map()
 
-  recipes.map((recipe) =>
+  recipes = recipes.map((recipe) => {
     recipe.components.map((component) => {
-      var ingredient_recipes = ingredients.get(component.ingredient) || [];
-      ingredient_recipes.push(recipe);
-      ingredients.set(component.ingredient, ingredient_recipes);
-      return null;
+      var ingredient_recipes = ingredients.get(component.ingredient) || []
+      ingredient_recipes.push(recipe)
+      ingredients.set(component.ingredient, ingredient_recipes)
+      return null
     })
-  )
 
-  ReactDOM.render(<App recipes={recipes} ingredients={ingredients} />, document.getElementById('root'));
+    recipe.slug = slug(recipe.name)
+    return recipe
+  })
+
+  ReactDOM.render(<Sozzler recipes={recipes} ingredients={ingredients} />, document.getElementById('root'));
 }
-
-// let url = 'https://raw.githubusercontent.com/mikepartelow/sozzler-recipes/master/SozzlerApp/1.0.sozzler';
-// fetch(url)
-//   .then((res) => res.json())
-//   .then((recipes) => {
-//     gitrdone(recipes)
-// })
 
 gitrdone(recipes)
